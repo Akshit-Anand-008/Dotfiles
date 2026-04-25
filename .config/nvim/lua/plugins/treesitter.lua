73b2780d4349e5e35554066760d16e55f6a0dfe4
Akshit_Anand <akshitanand008@gmail.com>
2026-04-26 00:45:50 +0530

    Commit on 2026-04-26 00:45

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     opts = {
-        ensure_installed = { "c", "cpp", "python", "rust", "lua", "latex" },
+        ensure_installed = { "cpp", "python", "rust" },
         highlight = { enable = true },
         indent = { enable = true },
     },
