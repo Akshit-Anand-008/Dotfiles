a0bf5b8e3a524d7360188c5b06bf500cecb7d17e
Akshit_Anand <akshitanand008@gmail.com>
2026-04-27 23:21:08 +0530

    Commit on 2026-04-27 23:21

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,39 +1,37 @@
 return {
     "nvim-telescope/telescope.nvim",
     version = "*",
-
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
-
-    vim.keymap.set("n", "<leader>fm", function()
-        local pickers = require("telescope.pickers")
-        local finders = require("telescope.finders")
-        local conf = require("telescope.config").values
-        local actions = require("telescope.actions")
-        local action_state = require("telescope.actions.state")
-        local options = {
-            "1. Open Buffers",
-            "2. Recent Files",
-            "3. Find Files (Current Dir)",
-            "4. Find Files (From Home Directory)",
-            "5. Find Wiki File (By Name)",
-            "6. Search inside Wiki (Text Grep)",
-        }
-        pickers
-            .new({}, {
-                prompt_title = "Select Finder",
+    config = function()
+        local telescope = require("telescope")
+        telescope.setup({})
+        telescope.load_extension("fzf")
+        vim.keymap.set("n", "<leader>fm", function()
+            local pickers = require("telescope.pickers")
+            local finders = require("telescope.finders")
+            local conf = require("telescope.config").values
+            local actions = require("telescope.actions")
+            local action_state = require("telescope.actions.state")
+            local options = {
+                "1. Open Buffers",
+                "2. Recent Files",
+                "3. Find Files (Current Dir)",
+                "4. Find Files (From Home Directory)",
+                "5. Find Wiki File (By Name)",
+                "6. Search inside Wiki (Text Grep)",
+            }
+            pickers.new({}, {
+                prompt_title = "Telescope Hub",
                 finder = finders.new_table({
-
                     results = options,
-
                 }),
                 sorter = conf.generic_sorter({}),
                 attach_mappings = function(prompt_bufnr, _)
                     actions.select_default:replace(function()
                         actions.close(prompt_bufnr)
-
                         local selection = action_state.get_selected_entry()
                         local choice = selection[1]
                         if choice == options[1] then
@@ -52,7 +50,7 @@ return {
                     end)
                     return true
                 end,
-            })
-            :find()
-    end, { desc = "Telescope Hub" })
+            }):find()
+        end, { desc = "Telescope Hub" })
+    end,
 }
