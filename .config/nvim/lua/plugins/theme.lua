4917138194cbe9689ea46df91271f598d3cd74cd
Akshit_Anand <akshitanand008@gmail.com>
2026-04-28 22:06:54 +0530

    Commit on 2026-04-28 22:06

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -8,13 +8,4 @@ return {
             vim.cmd.colorscheme("tokyonight")
         end,
     },
-    -- {
-    --     "catppuccin/nvim",
-    --     priority = 1000,
-    --     opts = { flavour = "mocha", },
-    --     config = function(_, opts)
-    --         require("catppuccin").setup(opts)
-    --         vim.cmd.colorscheme("catppuccin")
-    --     end,
-    -- }
 }
