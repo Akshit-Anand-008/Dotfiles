47eeb81db91912048fad663c59f0d9fb5a020c52
Akshit_Anand <akshitanand008@gmail.com>
2026-04-29 11:54:22 +0530

    Commit on 2026-04-29 11:54

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -6,26 +6,31 @@ return {
         require("lualine").setup({
             options = { theme = "tokyonight", icons_enabled = vim.g.have_nerd_font },
             sections = {
-                lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = {
-                    { "filename", path = 1 },
-                    {
-                        function()
-                            local line = vim.api.nvim_win_get_cursor(0)[1] - 1
-                            local diagnostics = vim.diagnostic.get(1, { lnum = line })
-                            if #diagnostics == 0 then
-                                return ""
-                            end
-                            local msg = (diagnostics[1] and diagnostics[1].message) or "Unknown error"
-                            local max_width = 80 -- Adjust this number based on your screen size
-                            if #msg > max_width then
-                                return "  " .. string.sub(msg, 1, max_width) .. "..."
-                            end
-                            return "  " .. msg
-                        end,
-                        color = { fg = "#f7768e" },
-                    },
-                },
+                lualine_a = { "mode" },
+                lualine_b = { "filename" },
+                lualine_c = {},
+                -- lualine_c = {
+                --     {
+                --         function()
+                --             local line = vim.api.nvim_win_get_cursor(0)[1] - 1
+                --             local diagnostics = vim.diagnostic.get(1, { lnum = line })
+                --             if #diagnostics == 0 then
+                --                 return ""
+                --             end
+                --             local msg = (diagnostics[1] and diagnostics[1].message) or "Unknown error"
+                --             local max_width = 80 -- Adjust this number based on your screen size
+                --             if #msg > max_width then
+                --                 return "  " .. string.sub(msg, 1, max_width) .. "..."
+                --             end
+                --             return "  " .. msg
+                --         end,
+                --         color = { fg = "#f7768e" },
+                --     },
+                -- },
+                lualine_x = {},
+                lualine_y = { "diagnostics", "filetype" },
+                lualine_z = { "location" },
+
             },
         })
     end,
