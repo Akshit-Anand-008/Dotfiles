69ce1e207d5da11813035db315cad3f57ca7b252
Akshit_Anand <akshitanand008@gmail.com>
2026-05-06 23:47:46 +0530

    Commit on 2026-05-06 23:47

--- .config/nvim/lua/plugins/auto_save.lua
+++ .config/nvim/lua/plugins/auto_save.lua
@@ -1,26 +1,26 @@
--- return {
---     "pocco81/auto-save.nvim",
---     cmd = "ASToggle",
---     lazy = true,
---     opts = {
---         enabled = false,
---         execution_message = {
---             enabled = false,
---             message = function()
---                 return ""
---             end,
---         },
---         trigger_events = { "InsertLeave", "TextChanged" },
---         condition = function(buf)
---             local fn = vim.fn
---             if
---                 fn.getbufvar(buf, "&modifiable") == 1 and fn.getbufvar(buf, "&modified") == 1
---             then
---                 return true
---             end
---             return false
---         end,
---         write_all_buffers = false,
---         debounce_delay = 135,
---     }
--- }
+return {
+    --     "pocco81/auto-save.nvim",
+    --     cmd = "ASToggle",
+    --     lazy = true,
+    --     opts = {
+    --         enabled = false,
+    --         execution_message = {
+    --             enabled = false,
+    --             message = function()
+    --                 return ""
+    --             end,
+    --         },
+    --         trigger_events = { "InsertLeave", "TextChanged" },
+    --         condition = function(buf)
+    --             local fn = vim.fn
+    --             if
+    --                 fn.getbufvar(buf, "&modifiable") == 1 and fn.getbufvar(buf, "&modified") == 1
+    --             then
+    --                 return true
+    --             end
+    --             return false
+    --         end,
+    --         write_all_buffers = false,
+    --         debounce_delay = 135,
+    --     }
+}
