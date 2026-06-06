69dbcf7a2ffdfe300ca8af3d6bf56feb6c023e93
Akshit_Anand <akshitanand008@gmail.com>
2026-06-07 00:42:02 +0530

    Commit on 2026-06-07 00:42

--- .config/nvim/lua/snippets/cpp.lua
+++ .config/nvim/lua/snippets/cpp.lua
@@ -1,27 +1,37 @@
 local ls = require "luasnip"
-local sn = ls.snippet_node
 local s = ls.snippet
-local t = ls.text_node
 local i = ls.insert_node
-local d = ls.dynamic_node
-local c = ls.choice_node
-local rep = require("luasnip.extras").rep
+local f = ls.function_node
 local fmt = require("luasnip.extras.fmt").fmt
 
+local boom = function(arg)
+    parts = arg.split(",")
+    local ans = ""
+    for _, i in parts do
+        ans = ans .. i .. ":%i, "
+    end
+end
 
 ls.add_snippets(
     "cpp",
     {
-        s("for", fmt("for (int i = 0; i < {}; i++) {{ {} }}", { i(1), i(0) })),
+        s("for",
+            fmt(
+                "for (int i = 0; i < {}; i++) {{ {} }}",
+                { i(1), i(0) }
+            )
+        ),
 
-        s("pr", fmt(
-            [[printf("{var1}:%i, {var2}:%i \n ", {rep1}, {rep2});{final}]], {
-                var1 = i(1, "name1"),
-                var2 = i(2, "name2"),
-                rep1 = rep(1),
-                rep2 = rep(2),
-                final = i(0)
-            })
-        )
+        s("pr",
+            fmt(
+                [[printf("{}\n",{});]],
+                { f(function(parameters)
+                    local ans = ""
+                    local parts = vim.split(parameters[1][1], ",", { plain = true, trimempty = true })
+                    for _, i in ipairs(parts) do ans = ans .. i .. ":%i, " end
+                    return ans or ""
+                end, { 1 }), i(1) }
+            )
+        ),
     }
 )
--- .config/nvim/lua/snippets/snips.lua
+++ .config/nvim/lua/snippets/snips.lua
@@ -2,8 +2,10 @@ local ls = require "luasnip"
 local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
+local f = ls.function_node
 local extras = require("luasnip.extras")
 local rep = extras.rep
+local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("tex", {
     s("\\beg", {
@@ -12,3 +14,18 @@ ls.add_snippets("tex", {
         t({ "", "\\end{" }), rep(1), t("}"),
     })
 })
+
+ls.add_snippets(
+    "lua",
+    {
+        s("req",
+            fmt(
+                [[local {} = require "{}"]],
+                { f(function(import_name)
+                    local parts = vim.split(import_name[1][1], ".", { plain = true, trimempty = true })
+                    return parts[#parts] or ""
+                end, { 1 }), i(1) }
+            )
+        )
+    }
+)
