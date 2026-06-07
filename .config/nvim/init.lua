16155992694e98af53925b569384f14ae0d9180e
Akshit_Anand <akshitanand008@gmail.com>
2026-06-07 14:37:26 +0530

    Commit on 2026-06-07 14:37

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,11 @@ require("commands")
 require("autocommands")
 require("keymaps")
 
+-- File types
+vim.filetype.add({
+    extension = { v = "systemverilog" }
+})
+
 -- Lazy-nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
@@ -18,27 +23,3 @@ if not vim.uv.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
-
--- File types
-vim.filetype.add({
-    extension = { v = "systemverilog" }
-})
-
--- Diagnostics
-local notify_handler = vim.lsp.handlers["textDocument/publishDiagnostics"]
-vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
-    local filtered_diagnostics = {}
-    for _, diagnostic in ipairs(result.diagnostics) do
-        if diagnostic.severity < 3 then
-            table.insert(filtered_diagnostics, diagnostic)
-        end
-    end
-    result.diagnostics = filtered_diagnostics
-    notify_handler(err, result, ctx, config)
-end
-vim.diagnostic.config({
-    virtual_text = { severity = vim.diagnostic.severity.ERROR, wrap = true },
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-})
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,8 +1,29 @@
 return {
     "williamboman/mason-lspconfig.nvim",
-    opts = {},
     dependencies = {
         { "williamboman/mason.nvim", opts = {} },
         { "neovim/nvim-lspconfig" },
-    }
+    },
+    config = function()
+        require("mason-lspconfig").setup({})
+
+        local notify_handler = vim.lsp.handlers["textDocument/publishDiagnostics"]
+        vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, result, ctx, config)
+            local filtered_diagnostics = {}
+            for _, diagnostic in ipairs(result.diagnostics) do
+                if diagnostic.severity < 3 then
+                    table.insert(filtered_diagnostics, diagnostic)
+                end
+            end
+            result.diagnostics = filtered_diagnostics
+            notify_handler(err, result, ctx, config)
+        end
+
+        vim.diagnostic.config({
+            virtual_text = { severity = vim.diagnostic.severity.ERROR, wrap = true },
+            underline = true,
+            update_in_insert = false,
+            severity_sort = true,
+        })
+    end
 }
