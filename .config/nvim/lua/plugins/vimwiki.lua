7a24a927bf3cefe034bbaa4a94143c5649238757
Akshit_Anand <akshitanand008@gmail.com>
2026-06-24 11:37:43 +0530

    Commit on 2026-06-24 11:37

--- .config/nvim/lua/plugins/vimwiki.lua
+++ .config/nvim/lua/plugins/vimwiki.lua
@@ -5,14 +5,13 @@ return {
             path = vim.fn.expand("$WIKI_PATH"),
             syntax = "markdown",
             ext = ".md",
-        }, }
+        } }
         vim.g.vimwiki_global_ext = 1
     end,
     config = function()
-        -- vim.keymap.set('n', '<leader>ww', '<cmd>VimwikiIndex 1<CR>', { silent = true })
+        vim.keymap.set('n', '<leader>wh', '<cmd>VimwikiIndex 1<CR>', { silent = true })
         vim.keymap.set('n', '<leader>wi', '<cmd>VimwikiDiaryIndex 1<CR>', { silent = true })
         vim.keymap.set('n', '<leader>w<leader>w', '<cmd>VimwikiMakeDiaryNote 1<CR>', { silent = true })
-
         vim.api.nvim_create_autocmd("FileType", {
             pattern = { "vimwiki" },
             callback = function(args)
