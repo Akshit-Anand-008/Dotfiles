52a8c6d7be1fa00dfedd0c71c475e7e973504ba8
Akshit_Anand <akshitanand008@gmail.com>
2026-06-24 10:35:13 +0530

    Commit on 2026-06-24 10:35

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -2,7 +2,6 @@ return {
     { "mbbill/undotree" },
     { "mrcjkb/rustaceanvim" },
     { "nvim-lua/plenary.nvim" },
-    { "numToStr/Comment.nvim" },
     { "kylechui/nvim-surround" },
     { "lewis6991/gitsigns.nvim" },
     { "HiPhish/rainbow-delimiters.nvim" },
--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,15 +1,14 @@
 return {
     "kylechui/nvim-surround",
-    -- config = function()
-    --     (require)("nvim-surround").setup({
-    --         mappings = {
-    --             add = false,
-    --             delete = false,
-    --             change = false,
-    --             visual = "S",
-    --             add_line = false,
-    --             change_line = false,
-    --         },
-    --     })
-    -- end,
+    init = function()
+        vim.g.nvim_surround_no_mappings = true;
+    end,
+    config = function()
+        vim.keymap.set("i", "<C-g>s", "<Plug>(nvim-surround-insert)")
+        vim.keymap.set("n", "s", "<Plug>(nvim-surround-normal)")
+        vim.keymap.set("x", "s", "<Plug>(nvim-surround-visual)")
+        vim.keymap.set("n", "ss", "<Plug>(nvim-surround-normal-cur)")
+        vim.keymap.set("n", "ds", "<Plug>(nvim-surround-delete)")
+        vim.keymap.set("n", "cs", "<Plug>(nvim-surround-change)")
+    end,
 }
