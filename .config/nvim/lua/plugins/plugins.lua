b8f79b73df63b25689da0bbce432fdb8ff67fb10
Akshit_Anand <akshitanand008@gmail.com>
2026-06-24 12:45:06 +0530

    Commit on 2026-06-24 12:45

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -2,6 +2,7 @@ return {
     { "mbbill/undotree" },
     { "mrcjkb/rustaceanvim" },
     { "nvim-lua/plenary.nvim" },
+    { "numToStr/Comment.nvim" },
     { "kylechui/nvim-surround" },
     { "lewis6991/gitsigns.nvim" },
     { "HiPhish/rainbow-delimiters.nvim" },
