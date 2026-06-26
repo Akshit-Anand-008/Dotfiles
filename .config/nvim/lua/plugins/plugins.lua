3f7e2ce633c5efd5aa3fa91d19b29818a092000b
Akshit_Anand <akshitanand008@gmail.com>
2026-06-26 21:18:45 +0530

    Commit on 2026-06-26 21:18

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,6 +1,6 @@
 return {
-    { "mrcjkb/rustaceanvim" },
-    { "lewis6991/gitsigns.nvim" },
+    -- { "mrcjkb/rustaceanvim" },
+    -- { "lewis6991/gitsigns.nvim" },
     { "HiPhish/rainbow-delimiters.nvim" },
     { "numToStr/Comment.nvim",          opts = {} },
     { "windwp/nvim-autopairs",          opts = {} },
