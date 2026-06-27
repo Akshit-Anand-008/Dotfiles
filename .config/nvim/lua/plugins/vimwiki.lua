0dd5fa2c784bc30409efee020a78f62611e1a5d3
Akshit_Anand <akshitanand008@gmail.com>
2026-06-27 16:56:33 +0530

    Commit on 2026-06-27 16:56

--- .config/nvim/lua/plugins/vimwiki.lua
+++ .config/nvim/lua/plugins/vimwiki.lua
@@ -9,7 +9,7 @@ return {
         vim.g.vimwiki_global_ext = 1
     end,
     config = function()
-        vim.keymap.set('n', '<leader>w1', '<cmd>VimwikiIndex 1<CR>', { silent = true })
+        vim.keymap.set('n', '<leader>W', '<cmd>VimwikiIndex 1<CR>', { silent = true })
         vim.keymap.set('n', '<leader>wi', '<cmd>VimwikiDiaryIndex 1<CR>', { silent = true })
         vim.keymap.set('n', '<leader>w<leader>w', '<cmd>VimwikiMakeDiaryNote 1<CR>', { silent = true })
         vim.api.nvim_create_autocmd("FileType", {
@@ -17,6 +17,9 @@ return {
             callback = function(args)
                 pcall(vim.keymap.del, { "x", "o" }, "il", { buf = 0 })
                 vim.keymap.set("n", "<A-x>", "<Plug>VimwikiToggleListItem", { buf = 0 })
+                pcall(vim.keymap.del, "n", "<CR>", { buf = 0 })
+                vim.keymap.set("n", "<leader><leader>", "<Plug>VimwikiNormalizeLinkVisualCR", { buf = 0 })
+                vim.keymap.set("n", "<leader><leader>", "<Plug>VimwikiFollowLink", { buf = 0 })
             end
         })
     end
