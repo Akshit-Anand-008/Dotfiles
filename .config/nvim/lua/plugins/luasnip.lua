88c25ce3d69cbd5abe8a51b6b5f90809ce135f13
Akshit_Anand <akshitanand008@gmail.com>
2026-06-29 21:25:31 +0530

    Commit on 2026-06-29 21:25

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -10,11 +10,11 @@ return {
         vim.keymap.set({ "i", "s" }, "<C-k>", function()
             if ls.expand_or_jumpable() then ls.expand_or_jump() end
         end, { silent = true })
-        vim.keymap.set({ "i", "s" }, "<C-l>", function()
+        vim.keymap.set({ "i", "s" }, "<C-d>", function()
             if ls.jumpable(-1) then
                 ls.jump(-1)
-            else
-                vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Right>", true, false, true), "n", false)
+                -- else
+                --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Right>", true, false, true), "n", false)
             end
         end, { silent = true })
     end
